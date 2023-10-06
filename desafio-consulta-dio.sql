cdae626106de694ab78a0bec1435af1614449b1b
Isaias Santos <isaiassousa032@gmail.com>
2023-10-06 14:28:16 -0300

    feat : desafio de banco de dados concluído

---
+++ desafio-consulta-dio.sql
@@ -0,0 +1,67 @@
+﻿
+-- 1
+SELECT Nome, Ano
+FROM Filmes;
+
+--2
+SELECT Nome, Ano
+FROM Filmes
+ORDER BY Ano;
+
+--3
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Nome = 'De Volta Para o Futuro';
+
+--4
+SELECT Nome
+FROM Filmes
+WHERE Ano = 1997;
+
+--5
+SELECT Nome
+FROM Filmes
+WHERE Ano > 2000;
+
+--6
+SELECT Nome, Duracao
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao;
+
+--7
+SELECT Ano, COUNT(*) AS QuantidadeDeFilmes
+FROM Filmes
+GROUP BY Ano
+ORDER BY QuantidadeDeFilmes DESC;
+
+--8
+SELECT PrimeiroNome, UltimoNome
+FROM Atores
+WHERE Genero = 'M';
+
+--9
+SELECT PrimeiroNome, UltimoNome
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome;
+
+--10
+SELECT Filmes.Nome, Generos.Genero
+FROM 
+((FilmesGenero INNER JOIN Filmes ON FilmesGenero.IdFilme = Filmes.Id) 
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id)
+
+--11
+SELECT Filmes.Nome,Generos.Genero
+FROM
+((FilmesGenero INNER JOIN Filmes ON FilmesGenero.IdFilme = Filmes.Id)
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id)
+WHERE Generos.Genero = 'Mistério'
+
+--12
+SELECT Filmes.Nome, Atores.PrimeiroNome, Atores.UltimoNome, ElencoFilme.Papel
+FROM
+	((ElencoFilme INNER JOIN Filmes ON ElencoFilme.IdFilme = Filmes.Id)
+				INNER JOIN Atores ON ElencoFilme.Id = Atores.Id)
+				INNER JOIN Generos ON ElencoFilme.IdAtor = Generos.Id
